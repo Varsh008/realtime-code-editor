58fba71e7d24be6bca8d60b12b97dfd54745e729
Varsh008 <varshinigowda003@gmail.com>
2025-01-12 15:44:09 +0530

    initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import app from 'App.jsx';
+import app from '\src\App.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
